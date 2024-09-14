e490d8fe0ed3836d6fff60967ab248630098baa0
Caiã <caiaalvespires@gmail.com>
2024-09-15 10:35:50 +1200

    cadastrar meta

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const {select} = require ('@inquirer/prompts')
+const {select, input} = require ('@inquirer/prompts')
+
+let meta = {
+    value: 'Tomar 3L de água por dia',
+    checked: false,
+}
+
+let metas=[meta]
+
+const cadastrarMeta = async () =>{
+    const meta = await input ({ message:"Digite a meta:"})
+    if(meta.length==0){
+
+        console.log("Você precisa escrever uma meta.")
+        return
+
+        
+
+    }
+    metas.push(
+        {value: meta, checked: false }
+    )
+    
+
+} 
+
 const start = async () => {
+
 while(true){
     const opcao = await select ({
         message: "Menu>",
@@ -13,19 +39,29 @@ while(true){
                 value:"listar"
             },
             {
+                name: "Marcar metas como concluídas",
+                value: "marcar"
+            },
+            {
                 name:"Sair",
                 value: "sair"
-            }
+            },
+            
         ]
     })
 switch (opcao){
     case "cadastrar":
-        console.log("vamos cadastrar")
+       await cadastrarMeta()
+       console.log(metas)
         break
         case "listar":
             console.log ("vamos listar")
             break
+            case "marcar":
+                console.log ("vamos marcar metas como concluídas")
+                break
             case "sair":
+                console.log("Até a próxima!")
                 return
 }
         
